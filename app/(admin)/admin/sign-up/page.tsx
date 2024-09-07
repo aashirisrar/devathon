f2182d94711454bc35fa33453ff4508fd6c067db
Syed Muslim <muslim.snh@gmail.com>
2024-09-07 10:21:57 +0500

    adding ui components and sign up page for admin

---
+++ app/(admin)/admin/sign-up/page.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import Modal from "@/components/modals/modal";
+import Heading from "@/components/hero-heading";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = z.object({
+    username: z.string().min(2).max(25),
+    email: z.string().includes("@", { message: "Enter a valid email address" }),
+    password: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", values)
+      .then(() => {
+        toast.success("Success!");
+        registerModal.onClose();
+        loginModal.onOpen();
+      })
+      .catch((error) => {
+        toast.error("Something went wrong!");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const toggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [loginModal, registerModal]);
+
+  const description = (
+    <Heading
+      center
+      title="Welcome to Portfolio"
+      subtitle="Create an account!"
+    />
+  );
+
+  const bodyContent = (
+    <Form {...form}>
+      <form className="flex flex-col gap-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input disabled={isLoading} required {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input disabled={isLoading} {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  disabled={isLoading}
+                  {...field}
+                  required
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <div
+        className="text-neutral-500 text-center
+      mt-4 font-light"
+      >
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={toggle}
+            className="text-neutral-800
+          cursor-pointer hover:underline"
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      onSubmit={form.handleSubmit(onSubmit)}
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      body={bodyContent}
+      footer={footerContent}
+      description={description}
+    />
+  );
+};
+
+export default RegisterModal;
--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,20 +1,10 @@
 import type { NextAuthConfig } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import prisma from "@/lib/prismadb";
 
 export default {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
     CredentialsProvider({
       name: "credentials",
       credentials: {
