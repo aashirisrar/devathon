48b5d874946244d605f8da13b18996c1bfa76d9b
Syed Muslim <muslim.snh@gmail.com>
2024-09-07 12:51:08 +0500

    updated schema

--- app/actions/getSubscription.ts
+++ app/actions/getSubscription.ts
@@ -5,28 +5,25 @@ import { getCurrentUser } from "@/app/actions/getCurrentUser";
 const DAY_IN_MS = 86_400_000;
 
 export const getSubscription = async () => {
-    const currentUser = await getCurrentUser();
+  // const currentUser = await getCurrentUser();
+  // if (!currentUser) return false;
+  // const userSubscription = await prismadb.userSubscription.findUnique({
+  //     where: {
+  //         userId: currentUser?.id,
+  //     },
+  //     select: {
+  //         stripeSubscritionId: true,
+  //         stripeCurrentPeriodEnd: true,
+  //         stripeCustomerId: true,
+  //         stripePriceId: true,
+  //     },
+  // });
+  // if (!userSubscription) return false;
+  // const isValid =
+  //     userSubscription.stripePriceId &&
+  //     userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+  //     Date.now();
+  // return !!isValid;
 
-    if (!currentUser) return false;
-
-    const userSubscription = await prismadb.userSubscription.findUnique({
-        where: {
-            userId: currentUser?.id,
-        },
-        select: {
-            stripeSubscritionId: true,
-            stripeCurrentPeriodEnd: true,
-            stripeCustomerId: true,
-            stripePriceId: true,
-        },
-    });
-
-    if (!userSubscription) return false;
-
-    const isValid =
-        userSubscription.stripePriceId &&
-        userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
-        Date.now();
-
-    return !!isValid;
+  return true;
 };
