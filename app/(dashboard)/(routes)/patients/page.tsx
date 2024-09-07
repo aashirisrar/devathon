9d68bb8e3a55602062cd386416e9f5f5d6c82575
Syed Muslim <muslim.snh@gmail.com>
2024-09-07 13:21:54 +0500

    sexy

--- app/(dashboard)/(routes)/patients/page.tsx
+++ app/(dashboard)/(routes)/patients/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
 
 const page = async () => {
+  const currentUser = await getCurrentUser();
+  const patients = await getPatients(currentUser?.id);
+
   return <div>page</div>;
 };
 
